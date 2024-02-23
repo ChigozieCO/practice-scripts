bfb88b4a2045484e612f5d51cc81ccb5ea3eefb8
ChigozieCO <chiggy.nm@gmail.com>
2024-02-23 13:32:43 +0100

    Modify the letter count script to ensure an argument is supplied

--- charcount.sh
+++ charcount.sh
@@ -2,6 +2,13 @@
 
 #This is a simple script that takes an argument from the user and counts the number of letters in that word.
 
+#Ensure that an argument is passed by the user, if not supply usage instruction and exit.
+
+if [ $# == 0 ]; then
+	echo "Usage: $0 <word or "string">"
+	exit 1
+fi
+
 #To do this I used command substitution.
 #The command to check the character count is wc -m (or -c) so I first outputed (with echo with the -n flag to remove the newline so it doesn't count it as a character) the word the user supplied using the first argument $1.
 #This is passed as an input for the " tr -d " " " command that I used in removing the whitespace and so the white space won't be counted as a character in the command.
